e7bea0fd5ae0bf5b073dff2238a76d2cc5257709
Alan <alanicio98@outlook.com>
2020-10-13 14:25:05 -0500

    Ya se muestra presupuesto, presupuesto restante, se muestran gastos y el presupuesto cambia de color segun el porcentaje restante

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import Error from "./Error";
+import PropTypes from "prop-types";
 
-const Pregunta = () => {
+const Pregunta = ({
+  guardarPresupuesto,
+  guardarRestante,
+  actualizarPregunta,
+}) => {
+  const [cantidad, guardarCantidad] = useState(0);
+  const [error, guardarError] = useState(false);
+
+  /*
+    NOTA: Lo que viene de un input usualmente viene como string,a pesar ser type number por ejemplo
+  */
+  const definirPresupuesto = (e) => {
+    guardarCantidad(parseInt(e.target.value, 10));
+  };
+
+  const agregarPresupuesto = (e) => {
+    e.preventDefault();
+
+    // NaN = Not a Number
+    if (cantidad < 1 || isNaN(cantidad)) {
+      guardarError(true);
+      return;
+    }
+
+    guardarError(false);
+    guardarPresupuesto(cantidad);
+    guardarRestante(cantidad);
+    actualizarPregunta(false);
+  };
+
   return (
     <>
-      <h2>Coloca tu presupuesto</h2> 
+      <h2>Coloca tu presupuesto</h2>
 
-      <form>
+      {error ? <Error mensaje="El Presupuesto es incorrecto" /> : null}
+
+      <form onSubmit={agregarPresupuesto}>
         <input
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
         />
 
         <input
@@ -19,7 +53,13 @@ const Pregunta = () => {
         />
       </form>
     </>
-  )
-}
+  );
+};
+
+Pregunta.propTypes = {
+  guardarPresupuesto: PropTypes.func.isRequired,
+  guardarRestante: PropTypes.func.isRequired,
+  actualizarPregunta: PropTypes.func.isRequired,
+};
 
 export default Pregunta;
